e51082ec25380e121703fb27bdc2bd2d03f32238
Seth Chisamore <schisamo@opscode.com>
2012-03-06 21:24:25 -0500

    display download instructiosn for  centos-6.2 iso like other centos definitions.

--- centos-6.2/definition.rb
+++ centos-6.2/definition.rb
@@ -1,12 +1,12 @@
 require File.dirname(__FILE__) + "/../.centos/session.rb"
 
-iso = "CentOS-6.2-x86_64-bin-DVD1.iso"
-
 session =
   CENTOS_SESSION.merge( :boot_cmd_sequence =>
                         [ '<Tab> text ks=http://%IP%:%PORT%/ks.cfg<Enter>' ],
-                        :iso_file => iso,
+                        :iso_download_instructions =>
+                        "http://isoredirect.centos.org/centos/6/isos/x86_64/",
+                        :iso_file => "CentOS-6.2-x86_64-bin-DVD1.iso",
                         :iso_md5 => "26fdf8c5a787a674f3219a3554b131ca",
-                        :iso_src => "http://mirror.rackspace.com/CentOS/6.2/isos/x86_64/#{iso}" )
+                        :iso_src => "" )
 
 Veewee::Session.declare session
